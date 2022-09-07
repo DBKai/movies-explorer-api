1b8c1f657e83d78d6448264c1cfb2f9ace8b5182
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-09-07 22:49:21 +0500

    feat: added wrong routes handler

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const NotFoundError = require('../errors/not-found-error');
 const { movieRouters } = require('./movies');
 const { userRouters } = require('./users');
 
@@ -6,6 +7,9 @@ const indexRouter = express.Router();
 
 indexRouter.use('/users', userRouters);
 indexRouter.use('/movies', movieRouters);
+indexRouter.use('*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
 
 module.exports = {
   indexRouter,
