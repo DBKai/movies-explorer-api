8239a2ab8c7b57746eecd7a949870f92b5d49d48
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-09-07 22:46:43 +0500

    feat: changed movies router and controller

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,3 +1,6 @@
+const IncorrectDataError = require('../errors/incorrect-data-error');
+const NotFoundError = require('../errors/not-found-error');
+const ForbiddenError = require('../errors/forbidden-error');
 const { Movie } = require('../models/movie');
 
 // Возвращает все сохранённые текущим  пользователем фильмы
@@ -5,8 +8,68 @@ exports.getMoviesByUserId = async (req, res, next) => {
   try {
     const userId = req.user._id;
     const movies = await Movie.findById(userId);
+    if (!movies) {
+      throw new NotFoundError('Не найден фильм с указанным id');
+    }
     return res.send(movies);
   } catch (err) {
+    if (err.name === 'CastError') {
+      return next(new IncorrectDataError('Передан некорректный id пользователя'));
+    }
+    return next(err);
+  }
+};
+
+// Создаёт фильм с переданными в теле
+// country, director, duration, year, description, image, trailer,
+// nameRU, nameEN и thumbnail, movieId
+exports.createMovie = async (req, res, next) => {
+  try {
+    const {
+      country, director, duration, year, description, image, trailerLink,
+      thumbnail, owner, movieId, nameRU, nameEN,
+    } = req.body;
+    const movie = Movie.create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      thumbnail,
+      owner,
+      movieId,
+      nameRU,
+      nameEN,
+    });
+    return res.send(movie);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return next(new IncorrectDataError('Переданы некорректные данные при создании фильма'));
+    }
+    return next(err);
+  }
+};
+
+// Удаляет фильм с указанным id
+exports.deleteMovie = async (req, res, next) => {
+  try {
+    const { movieId } = req.params;
+    const movie = Movie.findOne(movieId);
+    if (!movie) {
+      throw new NotFoundError('Фильм с указанным id не найден');
+    }
+    const isOwner = movie.owner.toString() === req.user._id;
+    if (!isOwner) {
+      throw new ForbiddenError('Нельзя удалить чужой фильм');
+    }
+    await Movie.deleteOne({ _id: movieId });
+    return res.send(movie);
+  } catch (err) {
+    if (err.name === 'CastError') {
+      return next(new IncorrectDataError('Переданы некорректные при удалении фильма'));
+    }
     return next(err);
   }
 };
--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const moviesController = require('../controllers/movies');
+const { userIdValidation, createMovieValidation, movieIdValidation } = require('../middlewares/validation');
 
 const movieRouters = express.Router();
 
-movieRouters.get('/', moviesController.getMoviesByUserId);
+movieRouters.get('/', userIdValidation, moviesController.getMoviesByUserId);
+movieRouters.post('/', createMovieValidation, moviesController.createMovie);
+movieRouters.delete('/:movieId', movieIdValidation, moviesController.deleteMovie);
 
 module.exports = {
   movieRouters,
