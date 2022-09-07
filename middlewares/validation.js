26794d72945bf3f837c2ffdf2d29ff0b3303b689
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-09-07 22:48:34 +0500

    feat: added validation

---
+++ middlewares/validation.js
@@ -0,0 +1,45 @@
+const { celebrate, Joi } = require('celebrate');
+
+exports.userIdValidation = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24),
+  }),
+});
+
+exports.getCurrentUserValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+exports.updateUserValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+exports.movieIdValidation = celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().hex().length(24),
+  }),
+});
+
+exports.createMovieValidation = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required(),
+    trailerLink: Joi.string().required(),
+    thumbnail: Joi.string().required(),
+    owner: Joi.string().hex().length(24),
+    movieId: Joi.string().hex().length(24),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
