7074ca95161338a8f961472edf3ff088c81bc350
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-09-09 23:51:42 +0500

    feat: refactored index.js

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
+const { limiter } = require('./middlewares/rateLimiter');
 const { indexRouter } = require('./routes');
 const { errorsHandler } = require('./middlewares/errorsHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { corsOptionsDelegate } = require('./middlewares/corsOptions');
+const { PORT, DATABASE_ADDRESS } = require('./configuration');
 
-const { PORT = 3000 } = process.env;
 const app = express();
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
 
 app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(requestLogger);
+app.use(cors(corsOptionsDelegate));
 app.use(helmet());
 app.disable('x-powered-by');
 app.use(indexRouter);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorsHandler);
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/moviedb', {
+  await mongoose.connect(DATABASE_ADDRESS, {
     useNewUrlParser: true,
   });
 
